71696a273ed0324408df6b3627e877c28a8fe05f
Dayeol <dyjo.seoul@gmail.com>
2021-08-29 15:57:28 +0900

    first commit

---
+++ blog-frontend/src/pages/WritePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
+import WriteActionButtons from '../components/write/WriteActionButtons';
+import Responsive from '../components/common/Responsive';
+import EditorContainer from '../containers/write/EditorContainer';
+const WritePage = () => {
+  return (
+    <Responsive>
+      <EditorContainer />
+      <TagBoxContainer />
+      <WriteActionButtons />
+    </Responsive>
+  );
+};
+
+export default WritePage;
