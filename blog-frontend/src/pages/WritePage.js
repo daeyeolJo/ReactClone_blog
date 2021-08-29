088cd32bb37383affb516c0b9fbe8ffc088d7349
Dayeol <dyjo.seoul@gmail.com>
2021-08-29 20:57:23 +0900

    21.08.29

--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TagBoxContainer from '../containers/write/TagBoxContainer';
-import WriteActionButtons from '../components/write/WriteActionButtons';
+import WriteActionButtonsContainer from '../containers/write/WriteActionButtonsContainer';
 import Responsive from '../components/common/Responsive';
 import EditorContainer from '../containers/write/EditorContainer';
 const WritePage = () => {
@@ -8,7 +8,7 @@ const WritePage = () => {
     <Responsive>
       <EditorContainer />
       <TagBoxContainer />
-      <WriteActionButtons />
+      <WriteActionButtonsContainer />
     </Responsive>
   );
 };
